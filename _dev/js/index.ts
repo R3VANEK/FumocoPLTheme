d96b33149c835484a9284b2ebe765e4df4542d86
R3VANEK <janek2000-2000@wp.pl>
2023-08-03 14:46:56 +0200

    work section RWD + svg animations

--- _dev/js/index.ts
+++ _dev/js/index.ts
@@ -1,13 +1,3 @@
 import "../css/theme.scss";
 import './menu'
-interface test{
-    color:string,
-    numbero:number
-}
-
-let obj:test = {
-    color: "siema",
-    numbero:1
-}
-
-console.log(obj.color)
+import './work'
---
+++ _dev/js/work.ts
@@ -0,0 +1,5 @@
+import $ from "jquery";
+
+let containerWidth:number = $('.work-section__card-h')[0].scrollWidth;
+$('.work-section__bg-card').width(containerWidth);
+
